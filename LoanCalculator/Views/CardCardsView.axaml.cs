0ebf943fa1b061059664d58509e3a753d0dc8ff6
Error710 <jhunrey.canasa@ctu.edu.ph>
2025-04-19 00:54:47 +0800

    Add project files.

---
+++ LoanCalculator/Views/CardCardsView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LoanCalculator.Views;
+
+public partial class CardCardsView : UserControl
+{
+    public CardCardsView()
+    {
+        InitializeComponent();
+    }
+}
