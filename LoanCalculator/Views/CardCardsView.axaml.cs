372015c9db8a1cfb208a778d220cedd8f683289b
Error710 <jhunrey.canasa@ctu.edu.ph>
2025-04-19 10:17:33 +0800

    DONE: BASIC UI

--- LoanCalculator/Views/CardCardsView.axaml.cs
+++ LoanCalculator/Views/CardCardsView.axaml.cs
@@ -1,6 +1,9 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using Avalonia.Media.Imaging;
+using Avalonia.Platform;
+using System;
 
 namespace LoanCalculator.Views;
 
@@ -9,5 +12,28 @@ public partial class CardCardsView : UserControl
     public CardCardsView()
     {
         InitializeComponent();
+    }
+    public string CardTitle
+    {
+        get => carName.Text;
+        set => carName.Text = value;
+    }
+
+    public string CardPrice
+    {
+        get => price.Text;
+        set => price.Text = value;
+    }
+
+    public string CardImageSource
+    {
+        get => carImage.Source?.ToString();
+        set
+        {
+            var uri = new Uri(value);
+            var assets = AssetLoader.Open(uri);
+            carImage.Source = new Bitmap(assets);
+        }
+
     }
 }
