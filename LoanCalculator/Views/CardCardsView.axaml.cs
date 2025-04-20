62160b18a864c1cd67a9832e033d08566b36b7e5
Error710 <jhunrey.canasa@ctu.edu.ph>
2025-04-20 22:04:58 +0800

    DONE: PURCHASE WINDOW

--- LoanCalculator/Views/CardCardsView.axaml.cs
+++ LoanCalculator/Views/CardCardsView.axaml.cs
@@ -1,39 +1,45 @@
-using Avalonia;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
-using Avalonia.Media.Imaging;
-using Avalonia.Platform;
-using System;
+    using Avalonia;
+    using Avalonia.Controls;
+    using Avalonia.Markup.Xaml;
+    using Avalonia.Media.Imaging;
+    using Avalonia.Platform;
+    using System;
 
-namespace LoanCalculator.Views;
+    namespace LoanCalculator.Views;
 
-public partial class CardCardsView : UserControl
-{
-    public CardCardsView()
+    public partial class CardCardsView : UserControl
     {
-        InitializeComponent();
-    }
-    public string CardTitle
-    {
-        get => carName.Text;
-        set => carName.Text = value;
-    }
-
-    public string CardPrice
-    {
-        get => price.Text;
-        set => price.Text = value;
-    }
-
-    public string CardImageSource
-    {
-        get => carImage.Source?.ToString();
-        set
+        public CardCardsView()
         {
-            var uri = new Uri(value);
-            var assets = AssetLoader.Open(uri);
-            carImage.Source = new Bitmap(assets);
+            InitializeComponent();
+        }
+        public string CardTitle
+        {
+            get => carName.Text;
+            set => carName.Text = value;
+        }
+
+        public string CardPrice
+        {
+            get => price.Text;
+            set => price.Text = value;
+        }
+
+        public string CardImageSource
+        {
+            get => carImage.Source?.ToString();
+            set
+            {
+                var uri = new Uri(value);
+                var assets = AssetLoader.Open(uri);
+                carImage.Source = new Bitmap(assets);
+            }
+
+        }
+        private void onPurchase(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var purchaseWindow = new PurchaseWindow();
+            purchaseWindow.Show();
         }
 
     }
-}
---
+++ LoanCalculator/Views/PurchaseWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace LoanCalculator.Views;
+
+public partial class PurchaseWindow : Window
+{
+    public PurchaseWindow()
+    {
+        InitializeComponent();
+    }
+}
